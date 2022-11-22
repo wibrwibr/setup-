feacd30cf3d8778fdfc5866ce356a39d9eb34f09
wibrwibr <118586650+wibrwibr@users.noreply.github.com>
2022-11-22 23:31:20 +0600

    Create Setup.sh

---
+++ Setup.sh
@@ -0,0 +1,111 @@
+clear
+
+echo "
+
+ █████╗ ██████╗ ██╗██████╗                         
+██╔══██╗██╔══██╗██║██╔══██╗                        
+███████║██████╔╝██║██████╔╝                        
+██╔══██║██╔══██╗██║██╔══██╗                        
+██║  ██║██████╔╝██║██║  ██║                        
+╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═╝                        
+                                                   
+███████╗██╗   ██╗███╗   ███╗ ██████╗ ███╗   ██╗    
+██╔════╝██║   ██║████╗ ████║██╔═══██╗████╗  ██║    
+███████╗██║   ██║██╔████╔██║██║   ██║██╔██╗ ██║    
+╚════██║██║   ██║██║╚██╔╝██║██║   ██║██║╚██╗██║    
+███████║╚██████╔╝██║ ╚═╝ ██║╚██████╔╝██║ ╚████║    
+╚══════╝ ╚═════╝ ╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝   
+"
+echo "==================================="
+echo "      tools name : basic pkg install
+"
+echo "      author : Abir
+"
+echo "==================================="
+
+
+pkg update
+pkg upgrade
+pkg install python
+pkg install python2
+pkg install python3
+pkg install python-pip
+pkg install wget
+pkg install fish -y
+pkg install ruby
+pkg install crunch
+pkg install git
+pkg install dnsutils -y
+pkg install php
+pkg install per
+pkg install parallel
+pkg install nmap -y
+pkg install bash
+pkg install clang
+pkg install nano
+pkg install w3m -y
+pkg install figlet
+pkg install cowsay
+pkg install curl
+pkg install tar
+pkg install zip
+pkg install unzip
+pkg install net-tools
+pkg install tor -y
+pkg install wcalc
+pkg install openssl
+pkg install openssl-tool
+pkg install bmon -y
+pkg install unrar
+pkg install toilet
+pkg install proot -y
+pkg install vim -y
+pkg install vim-python -y
+pkg install ired
+pkg install goaccess
+pkg install golang
+pkg install tar
+pkg install kibi
+pkg install tsu
+pkg install tmux
+pkg install mtools
+pkg install file
+pkg install vis -y
+pkg install pass -y
+pkg install pick
+termux-chroot
+pkg install macchanger
+pkg install ninja
+pkg install elixir -y
+pkg install swift -y
+pkg install xmlstarlet
+pkg install fakeroot
+pkg install texinfo -y
+pkg install wren
+pkg install gatling
+60. pkg install cvs
+pkg install ffmpeg -y
+pkg install screen 
+pkg install neofetch 
+pkg install mariadb
+pkg install picolisp
+pkg install cmatrix -y
+pkg install dropbear -y
+pip2 install wget
+pip install bs4
+pip2 install bs4
+pip install requests
+pip2 install requests
+pip install mechanize
+pip2 install mechanize
+pip install php
+pip2 install php
+pip2 install php2
+pkg install binutils
+pkg install binutils-is-llvm
+pkg install coreutils
+pkg install cups
+pkg install lf
+pkg install lhasa
+pkg install lr
+pkg install mtools
